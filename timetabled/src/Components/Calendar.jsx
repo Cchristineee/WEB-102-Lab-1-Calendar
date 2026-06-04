c8c47a04e0efc98690d89f1d62eae90b3aa1cd67
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-04 09:44:17 -0400

    Fixed timestamps grid

--- timetabled/src/Components/Calendar.jsx
+++ timetabled/src/Components/Calendar.jsx
@@ -4,6 +4,7 @@ const Calendar = () => {
             <table>
                 <thead>
                     <tr>
+                        <th></th>
                         <th>Monday</th>
                         <th>Tuesday</th>
                         <th>Wednesday</th>
@@ -13,21 +14,47 @@ const Calendar = () => {
                         <th>Sunday</th>
                     </tr>
                 </thead>
-            </table>
-
-            <table>
+                
                 <tbody>
                     <tr>
                         <td className="time">8 am</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">9 am</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">10 am</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">11 am</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">12 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">1 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">2 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">3 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">4 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    </tr>
+                    <tr>
                         <td className="time">5 pm</td>
+                        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
                     </tr>
                 </tbody>
             </table>
