371efdf0cd23b72c7aa95bdfabd4466969675abe
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-04 08:51:20 -0400

    Initial commit

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css';
+import Calendar from './Components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Christine's Miami Trip</h1>
+      <h2>Trip ideas</h2>
+    
+    <Calendar />
+  
+    </div>
+  )
+}
+
+export default App
+
---
+++ timetabled/src/Components/Calendar.jsx
@@ -0,0 +1,9 @@
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <h1>Testing the calendar component</h1>
+        </div>
+    )
+ }
+
+ export default Calendar;
